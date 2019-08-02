7798f5d89da467ba1ed997640b3a862a6ed4c050
DiMeL1002 <52574437+DiMeL1002@users.noreply.github.com>
2019-08-02 19:50:59 +0300

    Доработка
    Компоненты main-nav и mobile-nav были обьеденены в один компонент main-nav.

--- src/common.blocks/main-nav/main-nav.js
+++ src/common.blocks/main-nav/main-nav.js
@@ -1,24 +1,29 @@
 export function mainNav() {
-    let dropdowns = document.querySelectorAll('.main-nav__dropdown');
-    let menu = document.querySelector('.main-nav');
+    let menu = $('.main-nav');
+    let hamburger = menu.children('.hamburger');
+    let menuList = menu.children('.main-nav__list');
+    let dropdowns = menu.find('.main-nav__item_dropdown');
 
-    for (let i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].addEventListener('click', function(e) {
-            let parent = this.closest('.main-nav');
-            let siblings = parent.querySelectorAll('.main-nav__inner-list');
-            for (let i = 0; i < siblings.length; i++) {
-                siblings[i].classList.remove('main-nav__inner-list_visible');
-            }
+    dropdowns.on('click', function() {
+        $(this).toggleClass('main-nav__item_opened');
+        $(this).siblings('.main-nav__item_dropdown').removeClass('main-nav__item_opened');
+    })
 
-            menu = this.closest('.main-nav__item');
-            let innerList = menu.querySelector('.main-nav__inner-list');
-            innerList.classList.toggle('main-nav__inner-list_visible');
-        })
-    }
+    $(document).on('click', function(e) {
+        if (!dropdowns.is(e.target) && dropdowns.has(e.target).length === 0) {
+            dropdowns.removeClass('main-nav__item_opened');
+        }
+    })
 
-    document.addEventListener('click', function(e) {
-        if (!menu.contains(e.target)) {
-            menu.querySelector('.main-nav__inner-list').classList.remove('main-nav__inner-list_visible');
+    hamburger.on('click', function() {
+        $(this).toggleClass('is-active');
+        menuList.toggleClass('main-nav__list_visible');
+    })
+    
+    $(document).on('click', function(e) {
+        if (!menu.is(e.target) && menu.has(e.target).length === 0) {
+            hamburger.removeClass('is-active');
+            menuList.removeClass('main-nav__list_visible');
         }
     })
 }
--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ import Chart from 'chart.js';
 import 'normalize.css';
 import './main.scss';
 
-import { mobileNav } from './common.blocks/mobile-nav/mobile-nav.js';
+// import { mobileNav } from './common.blocks/mobile-nav/mobile-nav.js';
 import { mainNav } from './common.blocks/main-nav/main-nav.js';
 import { openDropdown } from './common.blocks/dropdown/dropdown.js';
 import { dropdownDates } from './common.blocks/dropdown-dates/dropdown-dates.js';
 
 window.addEventListener('load', function() {
-    mobileNav();
+    // mobileNav();
     mainNav();
     openDropdown();
     dropdownDates();
