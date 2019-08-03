26a95e095f56a89ad71633ad67130a059d13616d
DiMeL1002 <52574437+DiMeL1002@users.noreply.github.com>
2019-08-03 18:44:42 +0300

    Доработка
    Плагин air-datepicker перенесен в node-modules.

--- src/main.js
+++ src/main.js
@@ -3,20 +3,20 @@ import "popper.js"
 
 import 'nouislider';
 import 'nouislider/distribute/nouislider.css';
-import './library.blocks/air-datepicker/datepicker.min.js';
+
+import 'air-datepicker'
+import 'air-datepicker/dist/css/datepicker.css';
 
 import Chart from 'chart.js';
 
 import 'normalize.css';
 import './main.scss';
 
-// import { mobileNav } from './common.blocks/mobile-nav/mobile-nav.js';
 import { mainNav } from './common.blocks/main-nav/main-nav.js';
 import { openDropdown } from './common.blocks/dropdown/dropdown.js';
 import { dropdownDates } from './common.blocks/dropdown-dates/dropdown-dates.js';
 
 window.addEventListener('load', function() {
-    // mobileNav();
     mainNav();
     openDropdown();
     dropdownDates();
