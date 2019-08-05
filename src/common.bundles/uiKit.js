890bc48bc61551cd7e92c3d7abb025aaad9db7c0
DiMeL1002 <52574437+DiMeL1002@users.noreply.github.com>
2019-08-05 16:33:35 +0300

    Доработка
    Все компоненты относящиеся к странице UiKit были перенесы непосредственно в этот компонент.

--- src/common.bundles/uiKit.js
+++ src/common.bundles/uiKit.js
@@ -1,1 +1,19 @@
+import 'nouislider';
+import 'nouislider/distribute/nouislider.css';
+
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.css'
+
 import './uiKit.scss';
+
+import { initBtnLike } from '../common.blocks/btn-like/btn-like.js';
+import { initCheckboxList } from '../common.blocks/checkbox-list/checkbox-list.js';
+import { initRangeSlider } from '../common.blocks/range-slider/range-slider.js';
+import { initRoomCard } from '../common.blocks/room-card/room-card.js';
+
+window.addEventListener('load', function() {
+    initBtnLike();
+    initCheckboxList();
+    initRangeSlider();
+    initRoomCard();
+})
